7a8c26aec8b50e0bd3d420b6858aaa6db3cabed0
deepcyrix <deep.cjc.corpuz@gmail.com>
2022-07-04 15:17:20 +0800

    subdomain approach Post model

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,9 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::domain('api.'. env('APP_URL'))
+                ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
@@ -49,4 +50,34 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
+    // public function map()
+    // {
+    //     $this->mapApiRoutes();
+
+    //     $this->mapAdminRoutes();
+
+    //     $this->mapWebRoutes();
+    // }
+    // protected function mapApiRoutes()
+    // {
+    //     Route::domain('api.backup-demo.test')
+    //         ->middleware('api')
+    //         ->namespace($this->namespace)
+    //         ->group(base_path('routes/api.php'));
+    // }
+
+    // protected function mapWebRoutes()
+    // {
+    //     Route::domain('backup-demo.test')
+    //         ->middleware('web')
+    //         ->namespace($this->namespace)
+    //         ->group(base_path('routes/web.php'));
+    // }
+    // protected function mapAdminRoutes()
+    // {
+    //     Route::domain('admin.backup-demo.test')
+    //         ->middleware('web')
+    //         ->namespace($this->namespace)
+    //         ->group(base_path('routes/admin.php'));
+    // }
 }
